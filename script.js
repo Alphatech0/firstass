fbe6d7ebf5ca22eb019732c4ae633142a016e6b7
alphatech <imamzyat17@gmail.com>
2022-09-12 19:21:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,112 @@
+// section 1
+
+
+
+// comment your java script code
+// They are two different ways of commenting in JS
+// 1) in-line comment
+/* this is a
+fdsf
+fwgs
+g
+gwgerg
+ggergg
+multi-line comment*/
+
+
+// section 2
+/* Data types:
+undefined, null, boolean, string, symbol, number, and object.
+*/
+
+// variable: this also can change and its flexible to assign another value to it
+var myName = "alphatech"
+
+myName = 9
+
+// let: it is flexible and can be changed
+let ourName = "teammate"
+
+// const: this varible diclaration can never change no matter what you assign it to
+const pi = 3.14
+
+// difference btw declaring varibles and assigning varribles
+// this is a declaring varible
+var a;
+// this is an assigning varible
+var b = 2;
+
+// initializing variables with an assignment operators
+var a = 9;
+// that means that (var a) is declaring the value but (= 9) is initializing the value
+
+// increament and decreament of numbers with remainder
+var myVar = 30;
+
+// increament
+// myVar = myVar + 1;
+// myVar++
+
+
+// decreament
+myVar--
+
+// remainder
+var remainder;
+remainder = 11 % 3;
+
+// escape characters
+/* \' single quote
+    \" double quote
+    \\ backslash
+    \n new line
+    \r carriage return
+    \t tab
+    \b backspace
+    \f form feed
+*/
+// concatinating string
+// var myStr = "this is the start. " + "this is the end."
+
+// constructing strings together after declaration and assingment
+// var myName = "alphatech";
+// var myStr = "My name is " + myName + " and I am well";
+// function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+//     var result = "";
+//     return result;
+// }
+// console.log(wordBlanks("dog", "big", "ran", "quickly"));
+
+
+// how to push into an array and u can use (unshift) to push to the beginning of the array
+// var ourArray = [["strings", 23, 56], ["bolu", 50, 70]]
+// ourArray.push(["alphatech", 100, 4])
+// console.log(ourArray)
+
+
+// how to remove from an array the last value
+// var myArray = ["strings", 39, 46, 70]
+// var removeFromMyArray = myArray.pop();
+
+
+// how to remove from an array the first value
+// var myArray = ["alphatech", 28, 442, 49]
+// var removeFromMyArray = myArray.shift();
+
+// var myList = [["rice", 3], ["beans" 5], ["semo", 9]]
+
+
+// how to create a function
+// function reusableFunction() {
+//     console.log("Assalamualeikum my country people");
+// }
+// reusableFunction;
+
+
+// passing information to a function with an argument
+// function ourFunctionWithArgs(s, c) {
+//     console.log(s - c);
+// }
+// ourFunctionWithArgs(40, 30);
+
+
